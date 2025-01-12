01c3cb70259e561975a4feffc3c876086578b127
Manish Karmakar <mrinalkarmakar565@gmail.com>
2025-01-12 11:19:54 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
-import Index from "./pages/Home";
+import Home from "./pages/Home";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +16,7 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Index />} />
+            <Route path="/" element={<Home />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
